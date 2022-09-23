27c62be8c8b0b1621172355dc0b43afeccc91696
annisaabening04 <annisabening0@gmail.com>
2022-09-23 11:09:53 +0700

    include & require

---
+++ Pertemuan1/Annisa_NavigasiTokoOnline/css/header_include.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title> Latihan Mandiri </title>
+</head>
+<body>
+    <div class="header">
+        <h1> Annisa Bening Ramadhani </h1>
+        <i> Latihan include & require </i>
+        <hr/>
+</div>
+        
---
+++ Pertemuan1/Annisa_NavigasiTokoOnline/css/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title> Latihan Mandiri </title>
+</head>
+<body>
+    <div class="header">
+        <h1> Annisa Bening Ramadhani </h1>
+        <i> Latihan include & require </i>
+        <hr/>
+</div>
+<div class="content">
+    <br/>
+    home / About Us 
+    <h1> Tentang saya </h1>
+    <p> Nama saya Annisa, dari sekolah SMKN 5 SURAKARTA. </p>
+</div>
+<div class ="footer">
+    <hr/>
+    Include & require
+</div>
+
+</body>
+</html>
