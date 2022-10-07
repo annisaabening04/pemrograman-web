fa00c1eaa2fcc861b0a9a7cdb912544ef2785854
annisaabening04 <annisabening0@gmail.com>
2022-10-07 11:12:35 +0700

    pertemuan1

---
+++ Pertemuan1/Annisa_NavigasiTokoOnline/header.php
@@ -0,0 +1,9 @@
+<div class="container">
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">ANNISA STORE</h1>
+                    <p class="lead fw-normal text-50 mb-0">With this shop homepage </p>
+                </div>
+            </div>
+        </header>
--- Pertemuan1/Annisa_NavigasiTokoOnline/index.php
+++ Pertemuan1/Annisa_NavigasiTokoOnline/index.php
@@ -57,42 +57,11 @@
             </nav>
         </header>
     </div>
-    <div class="container">
-        <header class="bg-dark py-5">
-            <div class="container px-4 px-lg-5 my-5">
-                <div class="text-center text-white">
-                    <h1 class="display-4 fw-bolder">ANNISA STORE</h1>
-                    <p class="lead fw-normal text-50 mb-0">With this shop homepage </p>
-                </div>
-            </div>
-        </header>
+<!-- Header -->
+     <?php include "header.php" ?>
 
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
-                <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="img/toko1.jpg" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="img/toko2.jpg" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="img/toko3.jpg" class="d-block w-100" alt="...">
-                </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-                </button>
-        </div>
+<!-- Content -->
+        <?php include "content.php" ?>
             <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
@@ -195,117 +164,7 @@
                 </div>
             </div>
 <!-- Footer -->
-            <div class="container my-5">
-                <!-- Footer -->
-                <footer class="text-center text-lg-start text-white" style="background-color: #ce954b">
-                    <!-- Grid container -->
-                    <div class="container p-4 pb-0">
-                        <!-- Section: Links -->
-                        <section class="">
-                            <!--Grid row-->
-                            <div class="row">
-                                <!-- Grid column -->
-                                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                                    <h6 class="text-uppercase mb-4 font-weight-bold">
-                                        Company name
-                                    </h6>
-                                    <p>
-                                        Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                    </p>
-                                </div>
-                                <!-- Grid column -->
-
-                                <hr class="w-100 clearfix d-md-none" />
-
-                                <!-- Grid column -->
-                                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                                    <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
-                                    <p>
-                                        <a class="text-white">MDBootstrap</a>
-                                    </p>
-                                    <p>
-                                        <a class="text-white">MDWordPress</a>
-                                    </p>
-                                    <p>
-                                        <a class="text-white">BrandFlow</a>
-                                    </p>
-                                    <p>
-                                        <a class="text-white">Bootstrap Angular</a>
-                                    </p>
-                                </div>
-                                <!-- Grid column -->
-
-                                <hr class="w-100 clearfix d-md-none" />
-
-                                <!-- Grid column -->
-                                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                                    <h6 class="text-uppercase mb-4 font-weight-bold">
-                                        Useful links
-                                    </h6>
-                                    <p>
-                                        <a class="text-white">Your Account</a>
-                                    </p>
-                                    <p>
-                                        <a class="text-white">Become an Affiliate</a>
-                                    </p>
-                                    <p>
-                                        <a class="text-white">Shipping Rates</a>
-                                    </p>
-                                    <p>
-                                        <a class="text-white">Help</a>
-                                    </p>
-                                </div>
-
-                                <!-- Grid column -->
-                                <hr class="w-100 clearfix d-md-none" />
-
-                                <!-- Grid column -->
-                                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                                    <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
-                                    <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                                    <p><i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
-                                    <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                                    <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-                                </div>
-                                <!-- Grid column -->
-                            </div>
-                            <!--Grid row-->
-                        </section>
-                        <!-- Section: Links -->
-
-                        <hr class="my-3">
-
-                        <!-- Section: Copyright -->
-                        <section class="p-3 pt-0">
-                            <div class="row d-flex align-items-center">
-                                <!-- Grid column -->
-                                <div class="col-md-7 col-lg-8 text-center text-md-start">
-                                </div>
-                                <!-- Grid column -->
-
-                                <!-- Grid column -->
-                                <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
-                                    <!-- Facebook -->
-                                    <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class=""></i></a>
-
-                                    <!-- Twitter -->
-                                    <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class=""></i></a>
-
-                                    <!-- Google -->
-                                    <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class=""></i></a>
-
-                                    <!-- Instagram -->
-                                    <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class=""></i></a>
-                                </div>
-                                <!-- Grid column -->
-                            </div>
-                        </section>
-                        <!-- Section: Copyright -->
-                    </div>
-                    <!-- Grid container -->
-                </footer>
-                <!-- Footer -->
-            </div>
+            <?php include "footer.php" ?>
         </div>
     </div>
 </body>
